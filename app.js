4ced2226d1fc22ec70fc47843f60c4b3126fb797
KadenWilson3 <kadentwilson@icloud.com>
2021-01-14 00:07:02 -0700

    Done with ninth step

---
+++ app.js
@@ -0,0 +1,35 @@
+var firstName = "Kaden"; //this is my first name
+
+const states = 50; //this is the total amount of states in the U.S.
+
+var sum = 5 + 4; //this is the sum when you add the numbers five and four.
+
+function sayHello () {
+    alert('Hello World!');
+}
+
+sayHello('Hello World!');
+
+var name1 = "Charles";
+
+var name2 = "Abby";
+
+var name3 = "James";
+
+var name4 = "John";
+
+
+function checkAge (name, age) {
+    if (age<21) {
+        alert("Sorry" + " " + name + ", you aren't old enough to view this page!");
+    } 
+}
+
+checkAge(name1, 21);
+
+checkAge(name2, 27);
+
+checkAge(name3, 18)
+
+checkAge(name4, 17)
+
